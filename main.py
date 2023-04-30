1d128ed2ca9a3a0f973670e42c2c52b95b306c55
keple <keplerde@gmail.com>
2023-04-30 11:12:20 +0200

    logic setup

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import turtle
+import pandas
 
 screen = turtle.Screen()
 screen.title("U.S. States Game")
@@ -7,12 +8,31 @@ screen.addshape(image)
 
 turtle.shape(image)
 
-answer_state = screen.textinput(title="Guess the State", prompt="What´s another state´s name?")
-print(answer_state)
+data = pandas.read_csv("50_states.csv")
+all_states = data.state.to_list()
+guessed_states = []
+
+while len(guessed_states) < 50:
+    answer_state = screen.textinput(title=f"{len(guessed_states)}/50 States Correct",
+                                    prompt="What´s another state´s name?").title()
+
+    if answer_state in all_states:
+        guessed_states.append(answer_state)
+        t = turtle.Turtle()
+        t.hideturtle()
+        t.penup()
+        state_data = data[data.state == answer_state]
+        t.goto(int(state_data.x), int(state_data.y))
+        t.write(answer_state)
+    # If answer_state is one of the states in all the states of the 50_states.csv
+    # If they got it right:
+    # Create a turtle to write the name of the state at the states x and y coord.
+
+screen.exitonclick()
+
 # def get_mouse_click_coor(x, y):
 #     print(x, y)
 #
 # turtle.onscreenclick(get_mouse_click_coor)
 
 # turtle.mainloop()
-# screen.exitonclick()
